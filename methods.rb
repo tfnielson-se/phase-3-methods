d18f41b4ab532915086e10b303fb549df0cbb866
Thomas <tfnielson.se@gmail.com>
2023-01-27 10:55:29 -0700

    commit tree 1a8cbb6673963d297959c0d6ba995b4ac3eb33cc

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+greet("Jimmy")
+
+def greet_with_default (name="programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default("Sunny")
+
+def add (num1, num2) 
+    num1 + num2
+end
+sum = add(1,2)
+sum
+
+def halve number
+    if number.class != Integer
+        return nil
+    end
+    return number / 2
+end
+halve("two")
